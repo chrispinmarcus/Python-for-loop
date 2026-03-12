dbd8f5e58424931a937dbdc13a740872df0124fc
ArseneMerci <harsenemerci@gmail.com>
2026-03-12 19:28:46 +0200

    Iterable

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
-for x in range(5):
-    for y in range(3):
-        print(f"({x},{y})")
+#Iterable
+shopping_cart = ["apple", "bread", "milk"]
+for x in "python":
+    print(x)
+for y in [1,2,3,4,5]:
+    print(y)
+for item in shopping_cart:
+    print(item)
