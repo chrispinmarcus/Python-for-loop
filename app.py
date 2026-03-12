cf452379310bbd06061d215f29d15f38f2b068c1
ArseneMerci <harsenemerci@gmail.com>
2026-03-12 19:54:24 +0200

    exercise

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-#infinite loop
-while True:
-    command = input(">")
-    print("ECHO",command)
-    if command.lower() == "quit":
-        break
-
+#Exercise
+count = 0
+for number in range(1, 100):
+    if number % 2 == 0 :
+        count += 1
+        print(number)
+else:
+    print(f"We have {count} even number!")
