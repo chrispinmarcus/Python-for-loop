27c24460e66c4db89babaa52f01b8eaffed04e10
ArseneMerci <harsenemerci@gmail.com>
2026-03-12 19:39:30 +0200

    while loop

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-#Iterable
-shopping_cart = ["apple", "bread", "milk"]
-for x in "python":
-    print(x)
-for y in [1,2,3,4,5]:
-    print(y)
-for item in shopping_cart:
-    print(item)
+command = ""
+while command.lower() != "quit":
+    command = input(">")
+    print("ECHO", command)
